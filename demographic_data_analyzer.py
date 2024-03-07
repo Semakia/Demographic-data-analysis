2463af26204b39ec0e4bfb245c00e88ab30ac499
Semakia <98928019+Semakia@users.noreply.github.com>
2024-03-07 10:10:49 +0100

    Update demographic_data_analyzer.py

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -5,7 +5,7 @@ def calculate_demographic_data(print_data=True):
     # Read data from file
     df = pd.read_csv('adult.data.csv')
 
-    # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
+    # How many of each race are represented in this dataset?
     race_count = df['race'].value_counts()
 
     # What is the average age of men?
